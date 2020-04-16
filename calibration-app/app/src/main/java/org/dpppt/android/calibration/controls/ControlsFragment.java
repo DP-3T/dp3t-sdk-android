68261754f4db5afdd66d4b622ee226bb8c29709c
simonroesch <30999902+simonroesch@users.noreply.github.com>
2020-04-16 17:01:38 +0200

    add missing import

--- calibration-app/app/src/main/java/org/dpppt/android/calibration/controls/ControlsFragment.java
+++ calibration-app/app/src/main/java/org/dpppt/android/calibration/controls/ControlsFragment.java
@@ -32,6 +32,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
